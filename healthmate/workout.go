444bb80c89502f6529afe73f05e3ff9da3407ee2
Matthew Vandergrift <mvandergrift@gmail.com>
2020-10-01 14:55:07 -0400

    Use lastupdate strategy if start/end dates are not supplied @mvandergrift Fixes #2

--- healthmate/workout.go
+++ healthmate/workout.go
@@ -1,5 +1,11 @@
 package healthmate
 
+import (
+	"log"
+
+	"github.com/mvandergrift/energy-sdk/model"
+)
+
 type Timezone string
 
 type WorkoutResult struct {
@@ -35,4 +41,41 @@ type Data struct {
 	ManualCalories float64 `json:"manual_calories"`
 	Steps          int64   `json:"steps"`
 	Distance       float64 `json:"distance"`
+}
+
+type Export interface {
+	Display()
+	ExportWorkout() (model.Workout, error)
+}
+
+func (s Series) Display() {
+	log.Printf("%v:%v [%v]", s.Date, s.Category, s.Startdate)
+}
+
+func (s Series) ExportWorkout() (model.Workout, error) {
+	reportedCalories := s.Workout.ManualCalories
+	reportedDistance := s.Workout.ManualDistance
+
+	if reportedCalories < 1 {
+		reportedCalories = s.Workout.Calories
+	}
+
+	if reportedDistance < 1 {
+		reportedDistance = s.Workout.Distance
+	}
+
+	w := model.Workout{
+		Date:       s.Startdate.Time(),
+		ActivityID: int(s.Category),
+		UserID:     1,
+		Duration:   s.Workout.Effduration,
+		StartTime:  s.Startdate.Time(),
+		EndTime:    s.Enddate.Time(),
+		Calories:   reportedCalories,
+		Distance:   reportedDistance,
+		Comment:    "Imported from Healthmate via energy-sdk",
+		ExternalID: &s.ID,
+	}
+
+	return w, nil
 }
--- model/workout.go
+++ model/workout.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"time"
-
-	"github.com/mvandergrift/energy-sdk/healthmate"
 )
 
 type Workout struct {
@@ -25,32 +23,4 @@ type Workout struct {
 
 func (w Workout) TableName() string {
 	return "user_activity"
-}
-
-func NewWorkoutFromHealthmate(h healthmate.Series) (Workout, error) {
-	reportedCalories := h.Workout.ManualCalories
-	reportedDistance := h.Workout.ManualDistance
-
-	if reportedCalories < 1 {
-		reportedCalories = h.Workout.Calories
-	}
-
-	if reportedDistance < 1 {
-		reportedDistance = h.Workout.Distance
-	}
-
-	w := Workout{
-		Date:       h.Startdate.Time(),
-		ActivityID: int(h.Category),
-		UserID:     1,
-		Duration:   h.Workout.Effduration,
-		StartTime:  h.Startdate.Time(),
-		EndTime:    h.Enddate.Time(),
-		Calories:   reportedCalories,
-		Distance:   reportedDistance,
-		Comment:    "Imported from Healthmate via energy-sdk",
-		ExternalID: &h.ID,
-	}
-
-	return w, nil
 }
