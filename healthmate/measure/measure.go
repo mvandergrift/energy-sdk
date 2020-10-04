bb9445ad28ea6348f4696fac2cababe115746fe5
Matthew Vandergrift <mvandergrift@gmail.com>
2020-10-04 05:11:13 -0400

    Populate weight/bf/hr measures from Healthmate api Fixes #6

---
+++ healthmate/measure/measure.go
@@ -0,0 +1,58 @@
+package measure
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/mvandergrift/energy-sdk/healthmate"
+	"github.com/mvandergrift/energy-sdk/model"
+)
+
+type Result struct {
+	Status int64 `json:"status"`
+	Body   struct {
+		Updatetime healthmate.Timestamp `json:"updatetime"`
+		Timezone   string               `json:"timezone"`
+		//	Measuregrps []Measuregrp         `json:"measuregrps"`
+		Measuregrps []struct {
+			Grpid        int64                `json:"grpid"`
+			Attrib       int64                `json:"attrib"`
+			Date         healthmate.Timestamp `json:"date"`
+			Created      healthmate.Timestamp `json:"created"`
+			Category     int64                `json:"category"`
+			Deviceid     string               `json:"deviceid"`
+			HashDeviceid string               `json:"hash_deviceid"`
+			Measures     []Measure            `json:"measures"`
+			Comment      string               `json:"comment"`
+		} `json:"measuregrps"`
+	} `json:"body"`
+}
+
+type Measure struct {
+	Value       int64 `json:"value"`
+	Type        int64 `json:"type"`
+	Unit        int64 `json:"unit"`
+	Date        healthmate.Timestamp
+	Grpid       int64
+	AttributeID int64
+}
+
+func (s Measure) String() string {
+	return fmt.Sprintf("Healthmate - measure\t%v\t%v\t%v\t[%v]", s.Date.Time().Local(), s.Type, s.Value, s.Unit)
+}
+
+func (s Measure) Export() (interface{}, error) {
+	// generate unique ExternalID based on groupID and typeID
+	cantorPairing := (s.Grpid+s.Type)*(s.Grpid+s.Type+1)/2 + s.Type
+
+	w := model.Measure{
+		UserID:      1,
+		Date:        s.Date.Time(),
+		ExternalID:  &cantorPairing,
+		MeasureID:   s.Type,
+		Value:       float64(s.Value) * math.Pow10(int(s.Unit)),
+		AttributeID: s.AttributeID,
+	}
+
+	return w, nil
+}
